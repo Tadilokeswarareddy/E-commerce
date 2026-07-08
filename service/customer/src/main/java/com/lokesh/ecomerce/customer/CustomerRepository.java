bb9ddbe4a7c11a525acebe2e88c622ea7600b838
lokeshreddy <lokeshreddytadi@gmail.com>
2026-07-08 17:33:08 +0530

    done with customer microservice

---
+++ service/customer/src/main/java/com/lokesh/ecomerce/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.lokesh.ecomerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer,String> {
+}
---
+++ service/customer/src/main/java/com/lokesh/ecomerce/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.lokesh.ecomerce.handler;
+import java.util.Map;
+
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {
+
+}
