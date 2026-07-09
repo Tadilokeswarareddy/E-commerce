f07099d168ab6183ded846d079433fbfda817703
lokeshreddy <lokeshreddytadi@gmail.com>
2026-07-09 15:11:02 +0530

    done with product microservice

---
+++ service/product/src/main/java/com/lokesh/ecommerce/product/ProductService.java
@@ -0,0 +1,36 @@
+package com.lokesh.ecommerce.product;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository repository;
+    private final ProductMapper mapper;
+
+    public Integer createProduct(
+            ProductRequest request
+    ) {
+        var product = mapper.toProduct(request);
+        return repository.save(product).getId();
+    }
+
+    public ProductResponse findById(Integer id) {
+        return repository.findById(id)
+                .map(mapper::toProductResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with ID:: " + id));
+    }
+
+    public List<ProductResponse> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::toProductResponse)
+                .collect(Collectors.toList());
+    }
+}
+
